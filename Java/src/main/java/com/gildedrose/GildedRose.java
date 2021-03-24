eb1f21f42b4f76a7dea5852d79bb6cdf95a56f09
Stefaan Vanderheyden <stefaan.vanderheyden@nuuvo.mobi>
2021-03-24 22:42:53 +0100

    Refactor code to improve readability: - Use constants for repeating strings; - Use a for-each loop instead of the index-based loop; - Simplify complex calculations with explicit values when possible; - merge nested ifs; - use positive if conditions over negations where possible;

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,59 +1,55 @@
 package com.gildedrose;
 
+import static java.util.Objects.requireNonNull;
+
 class GildedRose {
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        this.items = requireNonNull(items);
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (AGED_BRIE.equals(item.name) || BACKSTAGE_PASSES.equals(item.name)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (BACKSTAGE_PASSES.equals(item.name)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                if (item.quality > 0 && !SULFURAS.equals(item.name)) {
+                    item.quality = item.quality - 1;
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!SULFURAS.equals(item.name)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+            if (item.sellIn < 0) {
+                if (AGED_BRIE.equals(item.name)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (BACKSTAGE_PASSES.equals(item.name)) {
+                        item.quality = 0;
+                    } else {
+                        if (item.quality > 0 && !SULFURAS.equals(item.name)) {
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
             }
